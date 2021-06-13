6dddec7c954f4f7583600994f969c2737088ad34
SushiTraash <deweihuang6@gmail.com>
2021-06-14 03:47:03 +0800

    p22 完成

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -3,7 +3,17 @@ package com.atguigu.gulimall.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * 使用rpc的服务中
+ *      导入openfeign依赖
+ *      编写接口，用于调用远程服务
+ *          每个方法都对应一个请求
+*       开启远程调用
+ *
+ */
+@EnableFeignClients(basePackages = "com.atguigu.gulimall.member.feign")
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallMemberApplication {
